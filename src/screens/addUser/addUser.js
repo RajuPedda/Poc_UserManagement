7801697555797b92c1b24be3d88c75c95eab6b8b
Raju <omniwyse@Omniwyses-MacBook-Air-2.local>
2018-12-17 14:51:27 +0530

    updated styles

--- src/screens/addUser/addUser.js
+++ src/screens/addUser/addUser.js
@@ -143,11 +143,13 @@ export default AddUser;
 
 export const styles = StyleSheet.create({
   container:{
-    flex: 1
+    flex: 1,
+    marginTop: 30,
   },
 
   inputItem: {
-    marginTop: 15
+    marginTop: 15,
+    marginLeft: 20
   },
   buttonContainer: {
     flex: 1, 
